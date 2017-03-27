b4346b4cc5acea7ce7bf426d735044e22cb58995
Stas Pazhoha <staconik@gmail.com>
2017-03-27 17:18:55 +0300

    CommandManager

--- src/VKBot/CommandManager.php
+++ src/VKBot/CommandManager.php
@@ -3,6 +3,7 @@
 class CommandManager
 {
     protected static $commands = [];
+    protected static $runned_commands = false;
 
     public static function addCommand(\VkBot\Command $command_obj)
     {
@@ -17,10 +18,16 @@ class CommandManager
             foreach ($names as $name) {
                 if (strpos($text, '/'.$name) !== false) {
                     self::runCommand($command, $message);
+                    self::$runned_commands = true;
                     break;
                 }
             }
         }
+    }
+
+    public static function existsRunnedCommands()
+    {
+        return self::$runned_commands;
     }
 
     protected static function getAllNames($command)
